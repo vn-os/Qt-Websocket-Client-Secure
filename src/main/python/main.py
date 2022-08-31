b062bda6436c6240add9118fe70022efe42f2ad7
Vic P <vic4key@gmail.com>
2022-08-31 11:05:28 +0700

    Initial

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 from PyQt5 import uic as UiLoader
 from PyQt5.QtGui import QColor
@@ -10,6 +10,10 @@ from picker import Picker
 
 class Window(QMainWindow):
 
+	m_prefs = {}
+	m_prefs_file_name = "prefs.json"
+
+	m_ws = None
 	m_endpoint = ""
 	m_message_text = ""
 	m_ssl_file_path = ""
@@ -21,20 +25,20 @@ class Window(QMainWindow):
 		return
 
 	def setup_ui(self):
-
-		# Load UI from file
+		# load ui from file
 		UiLoader.loadUi(self.app.get_resource("main.ui"), self)
-
-		# Signal & Slot
+		# signal & slot
+		self.actionSave.triggered.connect(self.on_triggered_menu_file_save)
 		self.actionExit.triggered.connect(self.on_triggered_menu_file_exit)
 		self.actionAbout.triggered.connect(self.on_triggered_menu_help_about)
 		self.txt_endpoint.textChanged.connect(self.on_changed_endpoint)
 		self.btn_connect.clicked.connect(self.on_clicked_button_connect)
 		self.btn_browse_ssl_file.clicked.connect(self.on_clicked_button_browse_ssl_file)
+		self.txt_message.textChanged.connect(self.on_changed_message)
 		self.btn_send_message.clicked.connect(self.on_clicked_button_send_message)
 		self.btn_clear_list_log.clicked.connect(self.on_clicked_button_clear_list_log)
-
-		return
+		# load and apply prefs
+		self.prefs_load_and_apply()
 
 	def is_default_style(self):
 		return QApplication.instance().style().metaObject().className() == "QWindowsVistaStyle"
@@ -44,40 +48,74 @@ class Window(QMainWindow):
 		item.setForeground(QColor(color))
 		self.list_log.addItem(item)
 
+	def prefs_get(self, name, default=""):
+		return self.m_prefs[name] if name in self.m_prefs.keys() else default
+
+	def prefs_set(self, name, value):
+		self.m_prefs[name] = value
+
+	def prefs_load_and_apply(self):
+		try:
+			with open(self.m_prefs_file_name, "r+") as f:
+				self.m_prefs = json.loads(f.read())
+				self.m_endpoint = self.prefs_get("endpoint")
+				self.m_ssl_file_path = self.prefs_get("ssl_file_path")
+				self.m_message_text = self.prefs_get("default_message")
+		except:
+			print("prefs not found or loading failed")
+		self.update_ui(None)
+
+	def prefs_save_to_file(self):
+		self.prefs_set("endpoint", self.m_endpoint)
+		self.prefs_set("ssl_file_path", self.m_ssl_file_path)
+		self.prefs_set("default_message", self.m_message_text)
+		with open(self.m_prefs_file_name, "w+") as f:
+			f.write(json.dumps(self.m_prefs, indent=4))
+
+	def update_ui(self, ws):
+		self.m_ws = ws
+		# enabled/disabled state
+		self.txt_message.setEnabled(not self.m_ws is None)
+		self.btn_send_message.setEnabled(not self.m_ws is None)
+		# edit-box ssl file path
+		self.txt_ssl_file_path.setText(os.path.basename(self.m_ssl_file_path))
+		self.txt_ssl_file_path.setToolTip(self.m_ssl_file_path)
+		# edit-box endpoint
+		self.txt_endpoint.setText(self.m_endpoint)
+		# edit-box message
+		self.txt_message.insertPlainText(self.m_message_text)
+
 	def on_triggered_menu_help_about(self):
 		self.about_dialog = AboutDlg(self.app)
 		response = self.about_dialog.exec_()
 		print(f"{self.about_dialog.__class__.__name__} {self.about_dialog.variable} {response}")
-		return
+
+	def on_triggered_menu_file_save(self):
+		self.prefs_save_to_file()
 
 	def on_triggered_menu_file_exit(self):
 		return self.close()
 
 	def on_changed_endpoint(self):
-		endpoint = self.txt_endpoint.text()
-		self.btn_connect.setEnabled(len(endpoint) > 0 and endpoint.startswith(("ws:", "wss:")))
-		return
+		self.m_endpoint = self.txt_endpoint.text()
+		self.btn_connect.setEnabled(self.m_endpoint.startswith(("ws:", "wss:")))
 
 	def on_clicked_button_connect(self):
-		self.m_endpoint = self.txt_endpoint.text()
 		print("on_clicked_button_connect", self.m_endpoint)
 		# your code here
-		return
 
 	def on_clicked_button_browse_ssl_file(self):
 		self.m_ssl_file_path = Picker.select_file(self, self.is_default_style())
-		self.txt_ssl_file_path.setText(os.path.basename(self.m_ssl_file_path))
-		self.txt_ssl_file_path.setToolTip(self.m_ssl_file_path)
+		self.update_ui(None)
 		print("on_clicked_button_browse_ssl_file", self.m_ssl_file_path)
-		return
+
+	def on_changed_message(self):
+		self.m_message_text = self.txt_message.toPlainText()
 
 	def on_clicked_button_send_message(self):
-		self.m_message_text = self.txt_message.toPlainText()
 		self.log(self.m_message_text, "green")
 		print("on_clicked_button_send_message", self.m_message_text)
 		# your code here
-		return
 
 	def on_clicked_button_clear_list_log(self):
 		self.list_log.clear()
-		return
