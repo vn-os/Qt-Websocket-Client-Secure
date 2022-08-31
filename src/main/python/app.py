9e8d052a0028bd2764876cbf829dc54ae675ce9f
Vic P <vic4key@gmail.com>
2022-08-31 17:15:07 +0700

    Initial

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -2,18 +2,27 @@ import os
 os.environ["PATH"] += os.pathsep if os.environ["PATH"][-1] != os.pathsep else ""
 os.environ["PATH"] += os.pathsep.join([os.getcwd()])
 
-import sys, qdarkstyle
+import sys
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 
 from main import Window
 
+# import qdarkstyle
+
+# import qtmodern.styles
+# import qtmodern.windows
+
 if __name__ == "__main__":
 	app = ApplicationContext()
-	app.app.setStyle("fusion")
-	# app.app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-	# app.PyQt5.QtWidgets.QStyleFactory.keys()
+	win = Window(app)
 
-	window = Window(app)
-	window.show()
+	app.app.setStyle("fusion")
+	
+	# app.app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+
+	# qtmodern.styles.dark(app.app)
+	# win = qtmodern.windows.ModernWindow(win)
+
+	win.show()
 
 	sys.exit(app.app.exec_())
